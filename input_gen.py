3f2cf5520418dd0c0f182669532490334b80fbf1
avivcaspi <avivcaspi@gmail.com>
2020-03-09 14:33:59 +0200

    added input generation and some utils i took from past hw and the git of the article also added testing notebook, to see that the generation of noise/ adding noise to image works well

---
+++ input_gen.py
@@ -0,0 +1,54 @@
+import torch
+import numpy as np
+
+
+def uniform_noise_gen(depth, height, width, std=1./10):
+    noise = torch.empty(depth, height, width).uniform_()
+    noise *= std
+    return noise
+
+
+def normal_noise_gen(depth, height, width, std=1./10):
+    noise = torch.empty(depth, height, width).normal_()
+    noise *= std
+    return noise
+
+
+def meshgrid_gen(height, width):
+    X, Y = np.meshgrid(np.arange(0, width)/float(width - 1), np.arange(0, height)/float(height - 1))
+    meshgrid = np.concatenate([X[None, :], Y[None, :]])
+    return torch.from_numpy(meshgrid)
+
+
+def get_noise(depth, height, width, method, noise_type='uniform', std=1./10):
+    """
+    create noise tensor with the shape as requested according to type and method given
+    :param depth: the depth of the noise tensor, if method is meshgrid - depth will be 2
+    :param height: the height of the noise tensor
+    :param width: the width of the noise tensor
+    :param method: can be 'noise' for normal or uniform noise, or 'meshgrid' for meshgrid
+    :param noise_type: can be 'uniform' or 'normal' for uniform or normal noise type
+    :param std: standard deviation of the noise
+    :return:
+    """
+    if method == 'noise':
+        if noise_type == 'uniform':
+            noise = uniform_noise_gen(depth, height, width, std)
+        elif noise_type == 'normal':
+            noise = normal_noise_gen(depth, height, width, std)
+        else:
+            assert False
+    elif method == 'meshgrid':
+        noise = meshgrid_gen(height, width)
+    else:
+        assert False
+    return noise
+
+
+def add_noise_to_tensor(img, noise_type='uniform', std=1./10):
+    assert img.dim() == 3
+    noise = get_noise(*img.shape, method='noise', noise_type=noise_type, std=std)
+    noisy_img = np.clip(img + noise, 0, 1)
+    return noisy_img, noise
+
+
---
+++ utils.py
@@ -0,0 +1,109 @@
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+from PIL import Image
+
+
+def tensors_as_images(tensors, nrows=1, figsize=(8, 8), titles=[],
+                      wspace=0.1, hspace=0.2, cmap=None):
+    """
+    Plots a sequence of pytorch tensors as images.
+
+    :param tensors: A sequence of pytorch tensors, should have shape CxWxH
+    """
+    assert nrows > 0
+
+    num_tensors = len(tensors)
+
+    ncols = math.ceil(num_tensors / nrows)
+
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize,
+                             gridspec_kw=dict(wspace=wspace, hspace=hspace),
+                             subplot_kw=dict(yticks=[], xticks=[]))
+    axes_flat = axes.reshape(-1)
+
+    # Plot each tensor
+    for i in range(num_tensors):
+        ax = axes_flat[i]
+
+        image_tensor = tensors[i]
+        assert image_tensor.dim() == 3  # Make sure shape is CxWxH
+
+        image = image_tensor.numpy()
+        image = image.transpose(1, 2, 0)
+        image = image.squeeze()  # remove singleton dimensions if any exist
+
+        # Scale to range 0..1
+        min, max = np.min(image), np.max(image)
+        image = (image-min) / (max-min)
+
+        ax.imshow(image, cmap=cmap)
+
+        if len(titles) > i and titles[i] is not None:
+            ax.set_title(titles[i])
+
+    # If there are more axes than tensors, remove their frames
+    for j in range(num_tensors, len(axes_flat)):
+        axes_flat[j].axis('off')
+
+    return fig, axes
+
+
+def load(path):
+    """Load PIL image."""
+    img = Image.open(path)
+    return img
+
+
+def get_image(path, imsize=-1):
+    """Load an image and resize to a cpecific size.
+    Args:
+        path: path to image
+        imsize: tuple or scalar with dimensions; -1 for `no resize`
+    """
+    img = load(path)
+
+    if isinstance(imsize, int):
+        imsize = (imsize, imsize)
+
+    if imsize[0]!= -1 and img.size != imsize:
+        if imsize[0] > img.size[0]:
+            img = img.resize(imsize, Image.BICUBIC)
+        else:
+            img = img.resize(imsize, Image.ANTIALIAS)
+
+    img_np = pil_to_np(img)
+
+    return img, img_np
+
+
+def pil_to_np(img_PIL):
+    """Converts image in PIL format to np.array.
+
+    From W x H x C [0...255] to C x W x H [0..1]
+    """
+    ar = np.array(img_PIL)
+
+    if len(ar.shape) == 3:
+        ar = ar.transpose(2, 0, 1)
+    else:
+        ar = ar[None, ...]
+
+    return ar.astype(np.float32) / 255.
+
+
+def np_to_pil(img_np):
+    """Converts image in np.array format to PIL image.
+
+    From C x W x H [0..1] to  W x H x C [0...255]
+    """
+    ar = np.clip(img_np * 255, 0, 255).astype(np.uint8)
+
+    if img_np.shape[0] == 1:
+        ar = ar[0]
+    else:
+        ar = ar.transpose(1, 2, 0)
+
+    return Image.fromarray(ar)
