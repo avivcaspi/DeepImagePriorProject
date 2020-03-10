1723f0b13ce2e53e654e306b0643f7c00e344fe1
Meccano <RoyTH100@gmail.com>
2020-03-10 17:15:04 +0200

    networks blocks and unet model with paramaters like in the supplemntary material

---
+++ models/blocks.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+
+
+REFLECTION_PADDING = 'reflection'
+BILINEAR = 'bilinear'
+NEAREST = 'nearest'
+UPSAMPLING_METHODS = [BILINEAR, NEAREST]
+
+
+# Conv -> BN -> LeakyReLU
+class ConvBlock(nn.Module):
+
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, norm_layer=nn.BatchNorm2d, leaky_slope=0.2, pad_type=REFLECTION_PADDING, weight_std=0.03):
+        nn.Module.__init__(self)
+
+        pad_size = (kernel_size - stride + 1) // 2
+        if pad_type == REFLECTION_PADDING:
+            self.pad = nn.ReflectionPad2d(pad_size)
+        else:
+            raise NotImplementedError()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride)
+        nn.init.normal_(self.conv.weight, mean=0.0, std=weight_std)
+
+        self.norm = norm_layer(out_channels)
+        self.activation = nn.LeakyReLU(leaky_slope)
+
+
+    def forward(self, x):
+        out = self.pad(x)
+        out = self.conv(out)
+        out = self.norm(out)
+        out = self.activation(out)
+        return out
+
+
+# (Conv -> BN -> LeakyReLU) -> (StridedConv -> BN -> LeakyReLU)
+class DownsampleBlock(nn.Module):
+
+
+    def __init__(self, in_channels, out_channels, kernel_size, scale_factor=2, norm_layer=nn.BatchNorm2d, leaky_slope=0.2, pad_type=REFLECTION_PADDING, weight_std=0.03):
+        nn.Module.__init__(self)
+
+        self.conv1 = ConvBlock(in_channels, out_channels, kernel_size, scale_factor, norm_layer, leaky_slope, pad_type, weight_std)
+        self.conv2 = ConvBlock(out_channels, out_channels, kernel_size, 1, norm_layer, leaky_slope, pad_type, weight_std)
+
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        return out
+
+
+# BN -> (Conv -> BN -> LeakyReLU) -> (Conv -> BN -> LeakyReLU) -> Upsample
+class UpsampleBlock(nn.Module):
+
+
+    def __init__(self, in_channels, out_channels, kernel_size, method, scale_factor=2, norm_layer=nn.BatchNorm2d, leaky_slope=0.2, pad_type=REFLECTION_PADDING, skip_depth=0, weight_std=0.03):
+        nn.Module.__init__(self)
+        assert method in UPSAMPLING_METHODS
+
+        self.method = method
+        self.skip_depth = skip_depth
+        in_channels += skip_depth
+
+        self.norm = norm_layer(in_channels)
+
+        self.conv1 = ConvBlock(in_channels, out_channels, kernel_size, 1, norm_layer, leaky_slope, pad_type, weight_std)
+        self.conv2 = ConvBlock(out_channels, out_channels, kernel_size, 1, norm_layer, leaky_slope, pad_type, weight_std)
+
+        self.upsample = nn.Upsample(scale_factor=scale_factor, mode=method)
+
+
+    def forward(self, x, skip_data=None):
+        if self.skip_depth > 0:
+            assert skip_data is not None
+            skip_data = nn.functional.interpolate(skip_data, size=x.shape[2:], mode=self.method)
+            x = torch.cat([x, skip_data], dim=1)
+
+        out = self.norm(x)
+        out = self.conv1(out)
+        out = self.conv2(out)
+        out = self.upsample(out)
+        return out
+
+
+# (Conv -> BN -> LeakyReLU)
+class SkipBlock(nn.Module):
+
+
+    def __init__(self, in_channels, out_channels, kernel_size, norm_layer=nn.BatchNorm2d, leaky_slope=0.2, pad_type=REFLECTION_PADDING, weight_std=0.03):
+        nn.Module.__init__(self)
+
+        self.conv = ConvBlock(in_channels, out_channels, kernel_size, 1, norm_layer, leaky_slope, pad_type, weight_std)
+
+
+    def forward(self, x):
+        out = self.conv(x)
+        return out
---
+++ models/unet.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from .blocks import *
+
+
+class UNet(nn.Module):
+
+
+    def __init__(self, in_channels, out_channels, nu, nd, ns, ku, kd, ks, up_method=BILINEAR, weight_std=0.03, leaky_slope=0.1):
+        nn.Module.__init__(self)
+
+        self.downs = []
+        self.ups = []
+        self.skips = []
+
+        # Create downsample part ('encoder')
+        last_depth = in_channels
+        for num_filters, kernel_size in zip(nd, kd):
+            layer = DownsampleBlock(last_depth, num_filters, kernel_size, weight_std=weight_std, leaky_slope=leaky_slope) if num_filters > 0 else None
+            self.downs.append(layer)
+            last_depth = num_filters
+
+        # Create upsample part ('decoder') in reverse order
+        for i, (num_filters, kernel_size) in reversed(list(enumerate(zip(nu, ku)))):
+            skip_depth = ns[i]
+
+            layer = UpsampleBlock(last_depth, num_filters, kernel_size, up_method, skip_depth=skip_depth, weight_std=weight_std, leaky_slope=leaky_slope) if num_filters > 0 else None
+            self.ups.append(layer)
+            last_depth = num_filters
+
+        self.ups = list(reversed(self.ups))
+
+        # Create skip connection blocks
+        for i, (num_filters, kernel_size) in enumerate(zip(ns, ks)):
+            layer = SkipBlock(nd[i], num_filters, kernel_size, weight_std=weight_std, leaky_slope=leaky_slope) if num_filters > 0 else None
+            self.skips.append(layer)
+
+
+        final_conv = nn.Conv2d(last_depth, out_channels, 1)
+        self.final = nn.Sequential(final_conv, nn.Sigmoid())
+
+
+    def forward(self, x):
+        main_data = x.clone()
+        skips_data = []
+
+        for i, down_layer in enumerate(self.downs):
+            main_data = down_layer(main_data)
+
+            if self.skips[i]:
+                skips_data.append(main_data.clone())
+            else:
+                skips_data.append(None)
+
+        for i, skip_layer in enumerate(self.skips):
+            if skip_layer:
+                skips_data[i] = skip_layer(skips_data[i])
+
+        for i, up_layer in reversed(list(enumerate(self.ups))):
+            main_data = up_layer(main_data, skips_data[i])
+
+        out = self.final(main_data)
+
+        return out
